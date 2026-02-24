a2cc4bdb448c1d97e7bd29c471656555eed85da5
Neo Sejwane <sejwanebls@gmail.com>
2026-02-24 17:47:05 +0200

    Connected Controller to Service and added CrossOrigin for Angular

--- src/main/java/com/medcare/backend/Controller/UserController.java
+++ src/main/java/com/medcare/backend/Controller/UserController.java
@@ -1,5 +1,27 @@
 package com.medcare.backend.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.medcare.backend.Model.User;
+import com.medcare.backend.Service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "*") 
+
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        //calls the service
+        return userService.registerUser(user);
+    }
 }
--- src/main/java/com/medcare/backend/Service/UserService.java
+++ src/main/java/com/medcare/backend/Service/UserService.java
@@ -1,5 +1,26 @@
+
 package com.medcare.backend.Service;
 
+import com.medcare.backend.Model.User;
+import com.medcare.backend.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; // The Scrambler
+import org.springframework.stereotype.Service;
+
+
+@Service
+
 public class UserService {
+    @Autowired
+     
+    private UserRepository userRepository;
+
+    private BCryptPasswordEncoder passwordEncoder =new BCryptPasswordEncoder();
+
+    public User registerUser(User user){
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        return userRepository.save(user);
+    }
 
 }
